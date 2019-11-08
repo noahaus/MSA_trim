b346c879e87ca66f95b9b19642722705bba10fa4
Noah Legall <40697188+noahaus@users.noreply.github.com>
2019-11-08 17:10:20 -0500

    Update MSA_trim.py

--- MSA_trim.py
+++ MSA_trim.py
@@ -16,16 +16,21 @@ ref_list = df.loc[0][1:df.shape[1]].tolist()
 
 # scan through each column, ignore the first column
 snp_columns = list(df)
-
 # let's get information about the reference sequence too
+
 #i is an index 
 #extract no. of samples
 column_num = 0
 sample_num = 0
 ambig_num = 0
+ambig_hist = []
 ref_num = 0
+ref_hist = []
 indel_num = 0
+indel_hist = []
 snp_num = 0
+snp_hist = []
+
 snp_prop = {}
 for i in snp_columns: 
     if "reference_pos" in i :
@@ -53,25 +58,28 @@ for i in snp_columns:
                 snp_prop[snp] = 1
             else:
                 snp_prop[snp] = snp_prop[snp]+1
-                
+        print("{},{},{},{}".format(indel_num,ambig_num,ref_num,snp_num))
+        ambig_hist.append(ambig_num)
+        ref_hist.append(ref_num)
+        indel_hist.append(indel_num)
+        snp_hist.append(snp_num) 
+        
         if len(snp_prop) == 1:
             del df[i]
         
         elif len(snp_prop) == 2:
             if (ambig_num/sample_num > ambig) or (indel_num/sample_num > indel) or (snp_num/sample_num > 0.9):
                 del df[i]
-            else:
-                continue
                 
         else:
-            if ((ambig_num + indel_num)/sample_num > 0.5) or (indel_num/sample_num > 0.5) or (ambig_num/sample_num > 0.5):
+            if ((ambig_num + indel_num)/sample_num > 0.5) or (indel_num/sample_num > indel) or (ambig_num/sample_num > ambig):
                 del df[i]
-            else:
-                continue
-    ambig_num = 0
-    ref_num = 0
-    indel_num = 0
-    snp_num = 0        
+       
+        ambig_num = 0
+        ref_num = 0
+        indel_num = 0
+        snp_num = 0 
+    
     snp_prop.clear()
     column_num += 1
         
